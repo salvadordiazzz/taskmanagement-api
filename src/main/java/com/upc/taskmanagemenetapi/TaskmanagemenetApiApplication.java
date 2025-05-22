4162dba22d1e198f0d953b6c27f73ecfc601d6aa
Salvador Diaz Aguirre <u202216148@upc.edu.pe>
2025-05-21 21:48:20 -0500

    estructura inicial del proyecto

---
+++ src/main/java/com/upc/taskmanagemenetapi/TaskmanagemenetApiApplication.java
@@ -0,0 +1,13 @@
+package com.upc.taskmanagemenetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagemenetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagemenetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/taskmanagemenetapi/TaskmanagemenetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.taskmanagemenetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagemenetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
